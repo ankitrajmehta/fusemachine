234528be5a809f3d58c90e74c0f210ce083687b7
ankitrajmehta <ankitrajmehta16@gmail.com>
2025-05-15 22:44:38 +0545

    Improved UI, added more sentiment change options

--- chat_app_backend/mysite/main.py
+++ chat_app_backend/mysite/main.py
@@ -4,15 +4,17 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from uuid import UUID, uuid4
+from groq import Groq
 
 
-
+# Setting Up MongoDB connection
 MONGODB_CONNECTION_STRING = os.environ["MONGODB_CONNECTION_STRING"]
 
 client = AsyncIOMotorClient(MONGODB_CONNECTION_STRING, uuidRepresentation="standard")
 db = client.messagelist
 message = db.messagess
 
+# Setting up FastAPI
 app = FastAPI()
 
 app.add_middleware(
@@ -23,13 +25,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class SentinmentInDB(BaseModel):
-    id: UUID = Field(default_factory=uuid4)
-    user_message: str
-    sentinment: str
-    timestamp: str
-    replied: bool = False
-
+# Defining the data models
 class Message(BaseModel):
     user_message: str
     timestamp: str
@@ -44,9 +40,6 @@ class MessageInDB(Message):
     replied: bool = False
 
 #Groq setup
-
-from groq import Groq
-
 
 llm_client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
@@ -82,28 +75,21 @@ async def convert_message(mood:str ,user_message: str) -> str:
     response = chat_completion
     return response.choices[0].message.content
 
+
+# API Endpoints
 @app.post("/messages", response_model=MessageInDB)
 async def create_message(message: Message):
     message_data = message.model_dump()
     message_data["sentinment"] = await calculate_sentinment(message_data["user_message"])
     message_data["id"] = str(uuid4())
+    message_data["replied"] = True
     result = await db.messagess.insert_one(message_data)
     created_message = await db.messagess.find_one({"_id": result.inserted_id})
-    message_data["replied"] = True
     if created_message:
         return MessageInDB(**created_message)
     else:
         raise HTTPException(status_code=404, detail="Message not Created")
 
-
-
-@app.get("/messages/{message_id}", response_model=MessageInDB)
-async def get_message(message_id: str):
-    message = await db.messagess.find_one({"_id": message_id})
-    if message:
-        return MessageInDB(**message)
-    else:
-        raise HTTPException(status_code=404, detail="Message not found")
 
 @app.get("/messages", response_model=list[MessageInDB])
 async def get_messages():
@@ -112,32 +98,17 @@ async def get_messages():
         messages.append(MessageInDB(**message))
     return messages
 
-@app.post("/messages/happy/", response_model=MessageInDB)
-async def create_message_happy(message: Message):
+@app.post("/messages/{mood}", response_model=MessageInDB)
+async def create_message_mood(mood: str, message: Message):
     message_data = message.model_dump()
     message_data["replied"] = False
     message_data["sentinment"] = await calculate_sentinment(message_data["user_message"])
     message_data["id"] = str(uuid4())
-    message_data["bot_message"] = await convert_message("happy", message_data["user_message"])
+    message_data["bot_message"] = await convert_message(mood, message_data["user_message"])
+    message_data["replied"] = True
     result = await db.messagess.insert_one(message_data)
     created_message = await db.messagess.find_one({"_id": result.inserted_id})
     if created_message:
-        message_data["replied"] = True
-        return MessageInDB(**created_message)
-    else:
-        raise HTTPException(status_code=404, detail="Message not Created")
-
-@app.post("/messages/sad/", response_model=MessageInDB)
-async def create_message_sad(message: Message):
-    message_data = message.model_dump()
-    message_data["replied"] = False
-    message_data["sentinment"] = await calculate_sentinment(message_data["user_message"])
-    message_data["id"] = str(uuid4())
-    message_data["bot_message"] = await convert_message("sad", message_data["user_message"])
-    result = await db.messagess.insert_one(message_data)
-    created_message = await db.messagess.find_one({"_id": result.inserted_id})
-    if created_message:
-        message_data["replied"] = True
         return MessageInDB(**created_message)
     else:
         raise HTTPException(status_code=404, detail="Message not Created")
